b89ea79717720f0158cdd663c4e4a576dc4b9cab
phyzicsz <phyzics.z@gmail.com>
2020-01-09 21:40:51 -0600

    permissions?

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ cd theodon
 #mvn clean package
 #mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=phyzicsz_theodon
+
+
