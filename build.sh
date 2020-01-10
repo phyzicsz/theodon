5b555ee0602a59c176fc346b0f6848d435893f72
phyzicsz <phyzics.z@gmail.com>
2020-01-09 21:22:20 -0600

    add key for sonar

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd theodon
 #mvn clean package
 #mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
-mvn clean package sonar:sonar -Pcoverage 
+mvn clean package sonar:sonar -Pcoverage -Dsonar.projectKey=phyzicsz_theodon
