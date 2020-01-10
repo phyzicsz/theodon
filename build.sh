85f419d65cec0578823ac880e0b5d36d4e263ea2
phyzicsz <phyzics.z@gmail.com>
2020-01-09 21:02:09 -0600

    wip sonarcube

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd theodon
 #mvn clean package
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
-
+#mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
+mvn clean package sonar:sonar -Pcoverage 
